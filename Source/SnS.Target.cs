8482520f7fdd5d39f93d14a3cea96d7b4ab18bbf
Leandro Shigaki <leandroshigaki@gmail.com>
2021-05-18 19:07:04 -0300

    Initial Commit

---
+++ Source/SnS.Target.cs
@@ -0,0 +1,16 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+using UnrealBuildTool;
+using System.Collections.Generic;
+
+public class SnSTarget : TargetRules
+{
+	public SnSTarget(TargetInfo Target) : base(Target)
+	{
+		Type = TargetType.Game;
+		DefaultBuildSettings = BuildSettingsVersion.V2;
+		bUsesSteam = true;
+
+		ExtraModuleNames.AddRange( new string[] { "SnS" } );
+	}
+}
